220cc002db70526ea78d120fdbfbd007f457cf81
Teezolusegun <teezolusegun@gmail.com>
2019-07-17 08:45:17 +0100

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+
+class Home extends Component{
+    render(){
+        return(
+            <div >Alpha News</div>
+        )
+    }
+}
+export default Home;
