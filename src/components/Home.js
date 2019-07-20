2a76bb5511fad94cb5687ffbd2bae724110c35d8
phython023 <phython023@gmail.com>
2019-07-20 03:47:25 +0100

    Adding-new-features

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,61 +6,67 @@ import './alpha.css'
 class Home extends Component{
     render(){
         return(
-            <div className='ctn-head'>
-            <div class="dropdown">
-                <button class="dropbtn">News</button>
+            <div className="ctn-head">
+            <div className="dropdown">
+                <button className="dropbtn">News</button>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">Tech</button>
+            <div className="dropdown">
+                <button className="dropbtn">Tech</button>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">Travel</button>
+            <div className="dropdown">
+                <button className="dropbtn">Travel</button>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">World News</button>
-                <div class="dropdown-content">
-                <a href="#">Africa</a>
-                <a href="#">Asia</a>
-                <a href="#">Europe</a>
-                <a href="*">south America</a>
+            <div className="dropdown">
+                <button className="dropbtn">World News</button>
+                <div className="dropdown-content">
+                <a href="">Africa</a>
+                <a href="">Asia</a>
+                <a href="">Europe</a>
+                <a href="">south America</a>
                 <a href=''>North America</a>
                 <a href=''>Australia</a>
                 <a href=''>Antarctica</a>
             </div>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">Tech</button>
+            <div className="dropdown">
+                <button className="dropbtn">Tech</button>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">Fashion</button>
+            <div className="dropdown">
+                <button className="dropbtn">Fashion</button>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">sports</button>
-                <div class="dropdown-content">
+            <div className="dropdown">
+                <button className="dropbtn">sports</button>
+                <div className="dropdown-content">
                 <a href="#">Football</a>
                 <a href="#">Boxing</a>
                 <a href="#">Other sports</a>
             </div>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">Entertainment</button>
+            <div className="dropdown">
+                <button className="dropbtn">Entertainment</button>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">Politics</button>
+            <div className="dropdown">
+                <button className="dropbtn">Politics</button>
             </div>
 
-            <div class="dropdown">
-                <button class="dropbtn">Our community</button>
+            <div className="dropdown">
+                <button className="dropbtn">Our community</button>
             </div>
-            </div>
+
+        <div className='inp'>
+        <input type='text' placeholder='search'></input>
+        </div>
+
+
+        </div>
 
         )
     }
