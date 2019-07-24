108119c0939eefc6a002c3fc1b3354180695b1ad
Mr_ Phython <phython023@gmail.com>
2019-07-24 16:59:11 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -4,10 +4,44 @@ import Home from './components/Home'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      articles: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://newsapi.org/v2/top-headlines?country=ng&apiKey=cd14e64e5ed84b1e8d15f944d3744eb9')
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    this.setState({
+     articles: myJson.articles
+    })
+  });
+  }
   render() {
+    console.log(this.state.articles, 'art')
     return (
-      <div >
+      <div className="news">
+      <div className='cl'>
        <Home />
+       {this.state.articles.map((item, index)=> {
+        return ( 
+          <div className='contents'>
+          <h2>
+           {item.title}
+         </h2>
+         <img src={item.urlToImage}></img>
+        <p>{item.description}</p>
+        <a href={item.url}>Read More</a>
+          </div>
+          
+         )
+          
+       })}
+       </div>
       </div>
     );
   }
